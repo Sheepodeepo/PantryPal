b3cc92a1c30faaf8202057a1ab007668c7f5b4de
alexa <alexanderlee014@gmail.com>
2025-04-15 20:50:04 -0700

    Restructured Meal Type enums to be defined and get Enum Values more effectively

--- Backend/src/main/java/com/PantryPal/model/MealType.java
+++ Backend/src/main/java/com/PantryPal/model/MealType.java
@@ -1,5 +1,23 @@
 package com.PantryPal.model;
 
+/**Enums class to display 4 options of Meal Types:
+ * Breakfast, Lunch, Dinner, Snack
+ *
+ */
 public enum MealType {
-    BREAKFAST, LUNCH, DINNER, SNACK
+    BREAKFAST("Breakfast"),
+    LUNCH("Lunch"),
+    DINNER("Dinner"),
+    SNACK("Snack");
+
+    private final String mealTypeName;
+
+    MealType(String mealType) {
+        this.mealTypeName = mealType;
+    }
+
+    public String getDisplayName() {
+        return mealTypeName;
+    }
+
 }
