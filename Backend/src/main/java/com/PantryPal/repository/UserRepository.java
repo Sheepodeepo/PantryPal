ba7401dd51d29ae4ee9fd31d317e99c51f2c3129
alexa <alexanderlee014@gmail.com>
2025-04-16 22:09:13 -0700

    Created basic UserRepository to act as a ListCrudRepository.

---
+++ Backend/src/main/java/com/PantryPal/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.PantryPal.repository;
+
+
+import com.PantryPal.model.User;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface UserRepository extends ListCrudRepository<User, Long> {
+    void findUserMyEmail(String email);
+    void deleteUserByEmail(String email);
+
+}
