f05c856443ea1eab8466028fa2fb4cca2b4dbd69
alexa <alexanderlee014@gmail.com>
2025-04-16 22:08:55 -0700

    Created basic User model object.

--- Backend/src/main/java/com/PantryPal/model/User.java
+++ Backend/src/main/java/com/PantryPal/model/User.java
@@ -1,4 +1,40 @@
 package com.PantryPal.model;
 
 public class User {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
