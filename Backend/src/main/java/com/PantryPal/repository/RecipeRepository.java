8b217e8bf2c2ccd79f808abf5e786e13bc275524
alexa <alexanderlee014@gmail.com>
2025-04-17 21:35:57 -0700

    Updated Recipe Repository to use custom SQL methods to store recipes in database

--- Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
+++ Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
@@ -3,10 +3,21 @@ package com.PantryPal.repository;
 import com.PantryPal.model.Recipe;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
-public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
-    Optional<Recipe> findByRecipeName(String name);
-    void deleteByRecipeName(String name);
+public interface RecipeRepository {
+    void save(Recipe recipe) throws SQLException;
+    void update(Recipe recipe);
+    void deleteById(long id);
+    Recipe findById(long id); //in ListCrudRepository returns Optional<Recipe>
+    List<Recipe> findAll();
+    void deleteAll();
 
 }
+//public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
+//    Optional<Recipe> findByRecipeName(String name);
+//    void deleteByRecipeName(String name);
+//
+//}
