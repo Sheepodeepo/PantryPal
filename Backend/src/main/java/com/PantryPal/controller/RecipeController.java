f91d5050ae39dd9d2f6561ff7ea473274e5258c4
alexa <alexanderlee014@gmail.com>
2025-05-01 20:44:49 -0700

    Updated Recipe Controller file with temporary comments

--- Backend/src/main/java/com/PantryPal/controller/RecipeController.java
+++ Backend/src/main/java/com/PantryPal/controller/RecipeController.java
@@ -4,6 +4,7 @@ import com.PantryPal.model.MealType;
 import com.PantryPal.model.Recipe;
 import com.PantryPal.repository.RecipeRepository;
 import com.PantryPal.service.AIService;
+import com.PantryPal.service.RecipePromptService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,12 @@ import java.util.Optional;
 public class RecipeController {
     private AIService aiService;
     private RecipeRepository repository;
+    private RecipePromptService recipePromptService;
 
-    public RecipeController(RecipeRepository repository, AIService aiService) {
+    public RecipeController(RecipeRepository repository, AIService aiService, RecipePromptService recipePromptService) {
         this.repository = repository;
         this.aiService = aiService;
+        this.recipePromptService = recipePromptService;
     }
 
     @GetMapping("/recipe/all")
@@ -26,19 +29,18 @@ public class RecipeController {
         return new ResponseEntity<>(repository.findAll(), HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/recipe/{id}")
-    public ResponseEntity<Recipe> findRecipeById(@PathVariable Long id){
-        return buildRecipeResponse(repository.findById(id));
-    }
+//    @GetMapping("/recipe/{id}")
+//    public ResponseEntity<Recipe> findRecipeById(@PathVariable Long id){
+//        return buildRecipeResponse(repository.findById(id));
+//    }
 
-    @GetMapping("/recipe")
-    public ResponseEntity<Recipe> findRecipeByName(@RequestParam String recipeName){
-        return buildRecipeResponse(repository.findByRecipeName(recipeName));
-    }
+//    @GetMapping("/recipe")
+//    public ResponseEntity<Recipe> findRecipeByName(@RequestParam String recipeName){
+//        return buildRecipeResponse(repository.findByRecipeName(recipeName));
+//    }
 
 //    @PostMapping("/recipe")
-//    public Recipe generateRecipe(@RequestParam String prompt,
-//                               @RequestParam MealType mealType) {
+//    public Recipe generateRecipe(@RequestParam MealType mealType) {
 //
 //    }
 
@@ -46,10 +48,18 @@ public class RecipeController {
     public ResponseEntity<Recipe> updateRecipe(@PathVariable Long id){
         return null;
     }
-    @DeleteMapping("/recipe")
-    public ResponseEntity<String> deleteRecipeByName(@RequestParam String recipeName){
-        repository.deleteByRecipeName(recipeName);
-        return new ResponseEntity<>("Deleted Recipe: " + recipeName + " successfully", HttpStatus.ACCEPTED);
+//    @DeleteMapping("/recipe")
+//    public ResponseEntity<String> deleteRecipeByName(@RequestParam String recipeName){
+//        repository.deleteByRecipeName(recipeName);
+//        return new ResponseEntity<>("Deleted Recipe: " + recipeName + " successfully", HttpStatus.ACCEPTED);
+//    }
+
+    private String createRecipePrompt(MealType mealType, String ingredients){
+        return recipePromptService.createGeminiPrompt(mealType, ingredients);
+    }
+
+    private String generateRecipeString(String recipePrompt){
+        return aiService.generateRecipe(recipePrompt);
     }
 
     private ResponseEntity<Recipe> buildRecipeResponse(Optional<Recipe> optionalRecipe){
