f7090682dafb5e2eb71b8f95731b1813b71557e2
alexa <alexanderlee014@gmail.com>
2025-05-04 23:32:38 -0700

    Use SpringData JDBC instead of custom Spring JDBC

--- Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
+++ Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
@@ -7,19 +7,17 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecipeRepository {
-    void save(Recipe recipe) throws SQLException;
-    void update(Recipe recipe);
-    void deleteById(long id);
-    Optional<Recipe> findById(long id); //in ListCrudRepository returns Optional<Recipe>
-    Optional<Recipe> findByRecipeName(String name);
-    List<Recipe> findAll();
-    void deleteAll();
-    void deleteByRecipeName(String name);
-
-}
-//public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
+//public interface RecipeRepository {
+//    void save(Recipe recipe) throws SQLException;
+//    void update(Recipe recipe);
+//    void deleteById(long id);
+//    Optional<Recipe> findById(long id); //in ListCrudRepository returns Optional<Recipe>
 //    Optional<Recipe> findByRecipeName(String name);
+//    List<Recipe> findAll();
+//    void deleteAll();
 //    void deleteByRecipeName(String name);
 //
 //}
+public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
+    void deleteByRecipeName(String name);
+}
