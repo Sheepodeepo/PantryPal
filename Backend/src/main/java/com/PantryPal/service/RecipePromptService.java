3fad9af348db0f5306ecca028f223dee59361865
alexa <alexanderlee014@gmail.com>
2025-05-07 20:05:15 -0700

    Updated comments for createGeminiPrompt method

--- Backend/src/main/java/com/PantryPal/service/RecipePromptService.java
+++ Backend/src/main/java/com/PantryPal/service/RecipePromptService.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RecipePromptService {
-    /**
-     * Create a recipe prompt to use in Gemini.
+    /** Create a recipe prompt to use in Gemini(AI Service Class).
      *
      * @param mealType: The type of meal to generate.
      * @param ingredients: Ingredients to be included in recipe prompt.
-     * @return String in the format: Title: ___ Ingredients: ___ Instructions: ___
+     * @return (String) => Here's a recipe ... \n#Title: ... #Ingredients: ... Instructions: ...
+     * where ingredients and instructions are separated by commas.
      */
     public String createGeminiPrompt(MealType mealType, String ingredients){
         StringBuilder promptBuilder = new StringBuilder("Create a ");
