7f23d7dbf7072ad71b64fe659dbd20416a565c5d
alexa <alexanderlee014@gmail.com>
2025-05-10 15:30:44 -0700

    Relocated gradle.yml file to Backend directory

---
+++ Backend/src/test/java/com/PantryPal/integration_tests/StoreRecipeInDbTest.java
@@ -0,0 +1,4 @@
+package com.PantryPal.integration_tests;
+
+public class StoreRecipeInDbTest {
+}
---
+++ Backend/src/test/java/com/PantryPal/repository/JdbcRecipeH2DBTest.java
@@ -0,0 +1,66 @@
+package com.PantryPal.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * https://docs.spring.io/spring-framework/reference/testing/support-jdbc.html
+ * https://www.baeldung.com/spring-jdbctemplate-testing
+ * https://docs.spring.io/spring-framework/reference/testing/support-jdbc.html
+ *
+ */
+class JdbcRecipeH2DBTest {
+
+    @Autowired
+    private JdbcClient jdbcClient;
+    private EmbeddedDatabase db;
+
+    @BeforeEach
+    void setUp(){
+        db = new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .addScripts("classpath:schema.sql", "classpath:recipe-test.sql")
+                .build();
+        jdbcClient = JdbcClient.create(db);
+    }
+
+    @AfterEach
+    void tearDown(){
+        db.shutdown();
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+        assertEquals(1,JdbcTestUtils.countRowsInTable(jdbcClient, "Recipe"));
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
