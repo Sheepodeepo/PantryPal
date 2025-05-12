73e4035ec112ed68a2887e2d245d27b1495ea9a3
alexa <alexanderlee014@gmail.com>
2025-05-12 14:54:58 -0700

    Add a dummy User Account in Users Table when launching.

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO Recipe (name, meal_type, ingredients, instructions, created_date, updated_date) --'2025-05-09'
 VALUES('Spaghetti', 'DINNER', 'Pasta, Spaghetti Sauce, Optional: Parmesan Cheese', 'Cook the pasta based on package time, Mix Cooked Pasta with Spaghetti Sauce, Add some Parmesan Cheese', '5/8/2025', null);
+
+INSERT INTO Users (email, password)
+VALUES('JoMama@epicsauce.com', 'whatisapassword1');
