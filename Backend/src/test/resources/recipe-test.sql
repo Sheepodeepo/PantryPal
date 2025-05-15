0692a4ea15a6a8a6768ba5d060ad6e7b0b72d5dc
alexa <alexanderlee014@gmail.com>
2025-05-14 19:10:55 -0700

    Remove SQL Files from test directory.

--- Backend/src/test/resources/recipe-test.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO Recipe(title,mealType,ingredients,instructions,createdDate)
-VALUES('Spaghetti', 'DINNER', 'Spaghetti, Ground Pork, Pasta Sauce', 'Cook spaghetti based on package details. Then add pasta with pasta sauce.', CURRENT_DATE);
-
