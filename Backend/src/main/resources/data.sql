d7e38eac94a3782e37e4621d78ef6236c41309f7
alexa <alexanderlee014@gmail.com>
2025-05-21 16:32:39 -0700

    Remove adding test users to Users DB.

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,8 +1,3 @@
 INSERT INTO Recipe (name, meal_type, ingredients, instructions, created_date, updated_date) --'2025-05-09'
 VALUES('Spaghetti', 'DINNER', 'Pasta, Spaghetti Sauce, Optional: Parmesan Cheese', 'Cook the pasta based on package time, Mix Cooked Pasta with Spaghetti Sauce, Add some Parmesan Cheese', '5/8/2025', null);
 
-INSERT INTO Users (email, password)
-VALUES('JoMama@epicsauce.com', 'whatisapassword1');
-
-INSERT INTO Users (email, password)
-VALUES('test', 'test');
