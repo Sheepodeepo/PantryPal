667b48f12711dd5a083a378ccb91710775cac7ea
alexa <alexanderlee014@gmail.com>
2025-05-29 18:31:13 -0700

    Added a user_id foreign key to Recipe Tables and reordered Creation/Deletion of Tables for foreign key dependencies.

--- Backend/src/main/resources/schema.sql
+++ Backend/src/main/resources/schema.sql
@@ -7,20 +7,23 @@
 
 --https://stackoverflow.com/questions/77722951/is-there-a-way-that-i-store-timestamp-with-time-zone-in-postgres-and-not-convert
 
-DROP TABLE IF EXISTS Recipe;
-CREATE TABLE Recipe(
+DROP TABLE IF EXISTS recipe; -- Delete non-dependent table first
+DROP TABLE IF EXISTS users;
+
+-- Create non-dependent table first
+CREATE TABLE users(
     id BIGSERIAL PRIMARY KEY,
+    email varchar(100) NOT NULL,
+    password varchar(255) NOT NULL
+);
+
+CREATE TABLE recipe(
+    id BIGSERIAL PRIMARY KEY,
+    user_id bigint references users(id),
     name varchar(255) NOT NULL,
     meal_type varchar(20),
     ingredients text NOT NULL,
     instructions text NOT NULL,
     created_date DATE,
     updated_date DATE
-);
-
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users(
-    id BIGSERIAL PRIMARY KEY,
-    email varchar(100) NOT NULL,
-    password varchar(255) NOT NULL
 );
