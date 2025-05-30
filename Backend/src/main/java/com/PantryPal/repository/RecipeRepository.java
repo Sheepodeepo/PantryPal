b14d07553ab2113e160de99f5ae016856cc64644
alexa <alexanderlee014@gmail.com>
2025-05-29 18:32:43 -0700

    Added custom findAllRecipes by UserId call to make sure that Users can only see's their own created recipes.

--- Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
+++ Backend/src/main/java/com/PantryPal/repository/RecipeRepository.java
@@ -1,6 +1,8 @@
 package com.PantryPal.repository;
 
 import com.PantryPal.model.Recipe;
+import com.PantryPal.model.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.sql.SQLException;
@@ -19,4 +21,10 @@ import java.util.Optional;
 //
 //}
 public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
+//    @Query("SELECT id, user_id, name, meal_type, ingredients, instructions, created_date, updated_date FROM recipe WHERE user_id = :user_id")
+    List<Recipe> findAllByUserId(long userId);
+
+//     Equivalent @Query annotation using JPQL
+//    @Query("SELECT r FROM Recipe r WHERE r.userId = :userId")
+//    List<Recipe> findRecipesByUserJPQL(long userId);
 }
