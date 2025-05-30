7d9ca10681e0434ec35012d2245f7dd3670f79c6
alexa <alexanderlee014@gmail.com>
2025-05-29 18:33:19 -0700

    Created a custom UserDetails object within Spring Security to store user_id to utilize user_id for Authentication.

---
+++ Backend/src/main/java/com/PantryPal/auth/MyUserDetails.java
@@ -0,0 +1,60 @@
+package com.PantryPal.auth;
+
+import com.PantryPal.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+    private Long id; // Your user_id
+    private String email; //email
+    private String password;
+    private List<GrantedAuthority> authorities;
+    private boolean enabled;
+    private boolean isAccountExpired;
+    private boolean isAccountLocked;
+    private boolean isCredentialsExpired;
+
+    public MyUserDetails(Long id, String email, String password, List<GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public MyUserDetails(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        // Assuming roles are stored in UserEntity, e.g., as a Set<String>
+        this.authorities = Collections.emptyList();
+//        this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")); // Example role
+        // Or if your UserEntity has roles:
+        // this.authorities = userEntity.getRoles().stream()
+        //                       .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+        //                       .collect(Collectors.toList());
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
