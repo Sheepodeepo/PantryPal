1648fc7b7740ad9df8f85525a3e7d968d85e94d3
alexa <alexanderlee014@gmail.com>
2025-05-29 18:34:47 -0700

    Messing with JwtSecretTest.

--- Backend/src/test/java/com/PantryPal/service/JwtSecretTest.java
+++ Backend/src/test/java/com/PantryPal/service/JwtSecretTest.java
@@ -11,8 +11,11 @@ public class JwtSecretTest {
     @Test
     public void generateSecretKey(){
         SecretKey key = Jwts.SIG.HS512.key().build(); // SIG.<Encryption Algos>.key(). Example algos: HS512, SHA256, etc.
+        String base64_key = DatatypeConverter.printBase64Binary(key.getEncoded());
         String encodedKey = DatatypeConverter.printHexBinary(key.getEncoded());
-        System.out.printf("\nKey = [%s]\n", encodedKey);
+        System.out.printf("\nHexBinary Key = [%s]\n", encodedKey);
+        System.out.printf("\nBase64Key = [%s]\n", base64_key);
+
         System.out.println(encodedKey);
     }
 }
