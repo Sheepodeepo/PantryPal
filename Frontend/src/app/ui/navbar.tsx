eac645b61aef9bc481793b83cebcf9bec33011f6
alexa <alexanderlee014@gmail.com>
2025-06-09 23:22:25 -0700

    Added a Navbar UI Component

--- Frontend/src/app/ui/navbar.tsx
+++ Frontend/src/app/ui/navbar.tsx
@@ -3,17 +3,23 @@ import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <div className="w-full">
-            <div className="flex items-center px-8 md:px-32 min-h-16 bg-sky-500 text-lg">
+        <div className="w-full bg-sky-500 text-xl">
+            {/* px-8 md:px-80 */}
+            <div className="flex items-center max-w-4xl mx-auto min-h-16">
                 <Link
-                    className="flex grow"
+                    className="flex grow justify-start px-4"
                     href={"/"}> 
                     PantryPal 
                 </Link>
 
-                <div className="flex grow justify-end">       
+                <div className="flex grow justify-end px-4">       
                     <Link 
-                        className=""
+                        className="px-4"
+                        href={"/register"}>
+                        Register 
+                    </Link>
+                    <Link 
+                        className="px-4"
                         href={"/login"}>
                         Login
                     </Link>
