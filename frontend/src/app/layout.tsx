19085ce0ca09f6938aa283d683c1975cedaa91b7
alexa <alexanderlee014@gmail.com>
2025-06-12 20:51:48 -0700

    Updated Layout to incorporate AuthProvider to root layout of web app

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,14 @@
+import { AuthProvider } from "@/app/context/AuthContext";
 import { geist } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
- 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geist.className} antialiased`} >
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
