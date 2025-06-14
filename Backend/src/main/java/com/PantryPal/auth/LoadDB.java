bb154f7ace8ad1a384ccbd8abdeffa7d25d5daf4
alexa <alexanderlee014@gmail.com>
2025-06-13 20:55:28 -0700

    Ensured correct recipe and user Ids set in LoadDB

--- Backend/src/main/java/com/PantryPal/auth/LoadDB.java
+++ Backend/src/main/java/com/PantryPal/auth/LoadDB.java
@@ -28,13 +28,19 @@ public class LoadDB {
     CommandLineRunner preLoadDB(RecipeRepository recipeRepository, UserRepository userRepository){
         return args -> {
             PasswordEncoder passwordEncoder = authConfig.passwordEncoder();
-            log.info("Preload User 1: {}", userRepository.save(new User("test", passwordEncoder.encode("test"))));
-            log.info("Preload User 2: {}", userRepository.save(new User("test@gmail", passwordEncoder.encode("longpasswordtest1"))));
-            log.info("Preload User 3: {}", userRepository.save(new User("test@test", passwordEncoder.encode("test"))));
+            User u1 = userRepository.save(new User("test", passwordEncoder.encode("test")));
+            User u2 = userRepository.save(new User("test@gmail", passwordEncoder.encode("longpasswordtest1")));
+            User u3 = userRepository.save(new User("test@test", passwordEncoder.encode("test")));
+            log.info("Preload User 1: {}", u1);
+            log.info("Preload User 2: {}", u2);
+            log.info("Preload User 3: {}", u3);
 
-            log.info("Preload Recipe 1: {}", recipeRepository.save(new Recipe(1,"dummy-name", MealType.BREAKFAST,"dummy-ingredients","dummy-instructions")));
-            log.info("Preload Recipe 2: {}", recipeRepository.save(new Recipe(1,"dummy-name2", MealType.BREAKFAST,"dummy-ingredients2","dummy-instructions2")));
-            log.info("Preload Recipe 3: {}", recipeRepository.save(new Recipe(2,"dummy-name3", MealType.BREAKFAST,"dummy-ingredients3","dummy-instructions3")));
+            log.info("Preload Recipe 1: {}", recipeRepository.save(new Recipe(u1.getId(),"dummy-name", MealType.BREAKFAST,"dummy-ingredients","dummy-instructions")));
+            log.info("Preload Recipe 2: {}", recipeRepository.save(new Recipe(u1.getId(),"dummy-name2", MealType.BREAKFAST,"dummy-ingredients2","dummy-instructions2")));
+            log.info("Preload Recipe 3: {}", recipeRepository.save(new Recipe(u2.getId(),"dummy-name3", MealType.BREAKFAST,"dummy-ingredients3","dummy-instructions3")));
+            log.info("Preload Recipe 4: {}", recipeRepository.save(new Recipe(u3.getId(),"dummy-name4", MealType.BREAKFAST,"dummy-ingredients4","dummy-instructions4")));
+            log.info("Preload Recipe 5: {}", recipeRepository.save(new Recipe(u3.getId(),"dummy-name5", MealType.BREAKFAST,"dummy-ingredients5","dummy-instructions5")));
+            log.info("Preload Recipe 6: {}", recipeRepository.save(new Recipe(u3.getId(),"dummy-name6", MealType.BREAKFAST,"dummy-ingredients6","dummy-instructions6")));
 
         };
     }
