025066f4ad7e037794b14a7021f08861444b0402
alexa <alexanderlee014@gmail.com>
2025-06-13 21:05:17 -0700

    Created basic version of UserHomePage displaying Recipes

--- frontend/src/app/ui/pages/UserHomePage.tsx
+++ frontend/src/app/ui/pages/UserHomePage.tsx
@@ -1,11 +1,19 @@
 import Navbar from "../navbar";
 
-export default function UserHomePage(){
+export default function UserHomePage({recipes} : any){
     return (
         <>
-            <div className="">
-                Logged In Home Page
-            </div>        
+        <div className="flex flex-col gap-y-4 py-4 max-w-4xl mx-auto min-h-16 ">
+            {recipes.map((recipe : any) => (
+              <div key={recipe.id} className="p-4 border rounded-lg bg-amber-400 shadow">
+                <h2 className="text-2xl font-bold">{recipe.name}</h2>
+                <p>{recipe.ingredients} </p>
+                <p>{recipe.instructions}</p>
+                <p>{!recipe.updatedDate ? recipe.createdDate :  recipe.updatedDate}</p>
+              </div>
+
+            ))}
+        </div> 
         </>
 
     )
