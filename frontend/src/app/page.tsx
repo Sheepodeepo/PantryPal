f662862e1598ce75769f0562904b2452a5298c1a
alexa <alexanderlee014@gmail.com>
2025-06-24 13:23:29 -0700

    Removed unnecessary imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { useState, useEffect } from "react"
-import PublicHomePage from "./ui/pages/PublicHomePage"
-import UserHomePage from "./ui/pages/UserHomePage";
 import HomePage from "./ui/pages/HomePage";
 import Navbar from "./ui/navbar";
 
