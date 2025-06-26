22601a2da8013cf0f87258e2f66b9c393e63f0c9
alexa <alexanderlee014@gmail.com>
2025-06-26 16:43:24 -0700

    Removed imports in UserRepository

--- Backend/src/main/java/com/PantryPal/repository/UserRepository.java
+++ Backend/src/main/java/com/PantryPal/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.PantryPal.repository;
 
 
+import com.PantryPal.model.Recipe;
 import com.PantryPal.model.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends ListCrudRepository<User, Long> {
