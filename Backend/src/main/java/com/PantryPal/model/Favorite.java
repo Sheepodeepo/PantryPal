0c63c8677886acce8fcb219caa3eb2404ffa7c01
alexa <alexanderlee014@gmail.com>
2025-06-26 16:48:47 -0700

    Created custom Favorite Entity to track favorite recipes

--- Backend/src/main/java/com/PantryPal/model/Favorite.java
+++ Backend/src/main/java/com/PantryPal/model/Favorite.java
@@ -1,4 +1,28 @@
 package com.PantryPal.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+
+@Table("favorites")
 public class Favorite {
+    private long user_id;
+    private long recipe_id;
+
+    public Favorite(){}
+
+    public Favorite(Long user_id, Long recipe_id) {
+        this.user_id = user_id;
+        this.recipe_id = recipe_id;
+    }
+
+    public long getRecipe_id() {
+        return recipe_id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
 }
