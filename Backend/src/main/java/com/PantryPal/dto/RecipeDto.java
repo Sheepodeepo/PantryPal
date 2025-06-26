7f38b7297283bbeca5d637fc5b0a9a4c72bb35fc
alexa <alexanderlee014@gmail.com>
2025-06-26 16:44:25 -0700

    Created a RecipeDto with recipeId

--- Backend/src/main/java/com/PantryPal/dto/RecipeDto.java
+++ Backend/src/main/java/com/PantryPal/dto/RecipeDto.java
@@ -1,4 +1,17 @@
 package com.PantryPal.dto;
 
 public class RecipeDto {
+    private long recipeId;
+
+    public RecipeDto(long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(long recipeId) {
+        this.recipeId = recipeId;
+    }
 }
