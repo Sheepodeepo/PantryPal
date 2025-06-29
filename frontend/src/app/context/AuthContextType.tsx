8585401979181edb8e7af0670ce77e9873f37a48
alexa <alexanderlee014@gmail.com>
2025-06-28 23:36:29 -0700

    Removed files

--- frontend/src/app/context/AuthContextType.tsx
+++
@@ -1,8 +0,0 @@
-interface AuthContextType{
-    user : any,
-    isAuthenticated: boolean,
-    loading : boolean,
-    login: (email: string, password: string) => Promise<boolean | void>;
-    signup: (email: string, password: string) => Promise<boolean | void>;
-    logout: () => Promise<void>;
-}
