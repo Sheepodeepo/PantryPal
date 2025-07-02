3702caf5fd326677b5b44f347ce8cfe6cbaa9ec7
alexa <alexanderlee014@gmail.com>
2025-07-01 17:25:31 -0700

    Test

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/ui/card";
+
+export default function Test(){
+    return (
+        <>
+            <Card>
+                Test
+                <div className="max-w-2xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis harum, quos optio delectus consequuntur quis molestias, officiis temporibus debitis vel porro ullam omnis impedit dolorem recusandae quas? Fugiat, exercitationem dicta!
+                </div>
+            </Card>
+        </>
+    )
+}
