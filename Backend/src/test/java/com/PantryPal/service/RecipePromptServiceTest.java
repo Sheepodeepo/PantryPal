c158785fb2bb5749e424eee2d7af6eda801163d2
alexa <alexanderlee014@gmail.com>
2025-07-06 23:40:18 -0700

    Updated RecipePromptService Unit Test

--- Backend/src/test/java/com/PantryPal/service/RecipePromptServiceTest.java
+++ Backend/src/test/java/com/PantryPal/service/RecipePromptServiceTest.java
@@ -10,7 +10,9 @@ class RecipePromptServiceTest {
     @Test
     void generateGeminiPromptWithoutIngredientsTest(){
         RecipePromptService genGeminiPrompt = new RecipePromptService();
-        String expectedGeminiPromptWithoutIngredients = "Create a BREAKFAST recipe in the format:<#Title: #Ingredients: #Instructions: . Use commas to separate the list of ingredients and instructions.>";
+        String expectedGeminiPromptWithoutIngredients = "Create a BREAKFAST recipe in the format:" +
+                "<#Title: #Ingredients: #Instructions: . Use commas to separate the list of ingredients. Use * to separate each line of instruction. Don't include ** in any part of the returned recipe.>";
+//        "<#Title: #Ingredients: #Instructions: . Use commas to separate the list of ingredients and instructions.>";
         assertEquals(expectedGeminiPromptWithoutIngredients, genGeminiPrompt.createGeminiPrompt(MealType.BREAKFAST,null));
     }
 
@@ -18,7 +20,7 @@ class RecipePromptServiceTest {
     void generateGeminiPromptWithIngredientsTest(){
         RecipePromptService genGeminiPrompt = new RecipePromptService();
         String expectedGeminiPromptWithoutIngredients = "Create a BREAKFAST recipe with the ingredients: bananas, apples, flour in the format:<#Title: #Ingredients: #Instructions: ."
-        + " Use commas to separate the list of ingredients and instructions.>";
+        + " Use commas to separate the list of ingredients. Use * to separate each line of instruction. Don't include ** in any part of the returned recipe.>";
         assertEquals(expectedGeminiPromptWithoutIngredients, genGeminiPrompt.createGeminiPrompt(MealType.BREAKFAST,"bananas, apples, flour"));
     }
 
