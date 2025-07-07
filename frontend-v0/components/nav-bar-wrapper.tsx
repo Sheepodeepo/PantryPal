b6e207f3f0a12b867932b1650ac824d6455fd820
alexa <alexanderlee014@gmail.com>
2025-07-07 13:53:00 -0700

    Removed v0 demo website

--- frontend-v0/components/nav-bar-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import NavBar from "@/components/nav-bar"
-import { getCurrentUser } from "@/lib/actions/auth-actions"
-
-export default async function NavBarWrapper() {
-  const user = await getCurrentUser()
-  return <NavBar user={user} />
-}
